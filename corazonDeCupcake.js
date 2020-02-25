cd204651667603f8e9796753fcc699276041b65a
eduardoMag <eduardobautista13@gmail.com>
2020-02-24 21:00:44 -0600

    rebuilding.

--- corazonDeCupcake.js
+++ corazonDeCupcake.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-//static middleware
+//middleware
 app.use(express.static(__dirname + '/public'));
 
-//setup handlebars view engine
-const handlebars = require('express3-handlebars').create({defaultLayout:'main' });
+//set up for handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
+//port
 app.set('port', process.env.PORT || 3000);
-//home page
+
+//routes
+// app.get('/', (req, res)=>{res.render('home');});
+
 app.get('/', function(req, res){
   res.render('home');
 });
 
-//comming soon page
-app.get('/comming-soon', function(req, res){
-  res.render('comming-soon');
-});
-
-//404 catch-all handler (middleware)
-app.use(function( req, res, next){
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
   res.status(404);
   res.render('404');
 });
-//500 error handler (middleware)
-app.use(function(req, res, next){
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http:localhost: '+app.get('port')+'; press Ctrl-C to terminate.');
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate' );
 });
